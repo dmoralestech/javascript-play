8d958a673bd1ea08c58901ee2e8265b376890fa4
darwinmorales <darwin.morales.tech@gmail.com>
2016-08-13 09:17:26 +1000

    closures comments

--- src/webapp/adv_js_ex/closure.js
+++ src/webapp/adv_js_ex/closure.js
@@ -10,15 +10,24 @@
 function foo() {
     var bar = "bar";
 
+
     function baz() {
+        /*
+         Here baz knows by lexical scope, ie it will find first in the current scope, if it doesn't find it,
+         it will look at the outer scope. In this case it's in the scope of foo().
+         */
         console.log(bar);
     }
 
-    bam(baz);
+    bam(baz); // bam is called in a separate function scope but with a reference to baz
 }
 
-function bam(baz) {
-    baz();
+
+function bam(baz) { // baz still here has a reference to the function foo() which has var bar
+            /*
+            It's not a copy of the lexical scope. It's a reference to the baz lexical scope.
+             */
+    baz(); // returns "bar".. this is where the closure is happening
 }
 
 foo();
---
+++ src/webapp/adv_js_ex/this_ex.js
@@ -0,0 +1,6 @@
+/**
+ * Created by darwinmorales on 13/08/2016.
+ */
+
+
+// Every function while executing has a reference to its current execution context called this.
