f78da72e63ba3c55984ae33a3f5b639f6e1322e8
darwinmorales <darwin.morales.tech@gmail.com>
2016-08-15 21:16:42 +1000

    closures and prototypes.

--- src/webapp/adv_js_ex/Prototypes.js
+++ src/webapp/adv_js_ex/Prototypes.js
@@ -5,3 +5,27 @@
 // each time a constructor is called  a new object is created
 
 // A constructor makes an object linked to its own prototype
+
+function Foo(who) {
+    this.me = who;
+}
+
+Foo.prototype.identify = function() {
+    return "I am " + this.me;
+};
+
+var a1 = new Foo("a1");
+var a2 = new Foo("a2");
+
+a2.speak = function() {
+    console.log("Hello " + this.identify());
+}
+
+console.log(a1.constructor === Foo);  //true
+console.log(a1.constructor === a2.constructor); //true
+console.log(a1.__proto__ === Foo.prototype); //true
+console.log(a1.__proto__ === a2.__proto__); //true
+
+console.log(a1.me);
+console.log(a2.me);
+
