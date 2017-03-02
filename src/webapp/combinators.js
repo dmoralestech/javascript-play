a0271c8ecfca501fc1d573b721b398291926e81d
dmorales <darwin.morales.tech@gmail.com>
2017-03-02 16:14:18 +1100

    combinators from reginald

--- src/webapp/combinators.js
+++ src/webapp/combinators.js
@@ -1,3 +1,5 @@
+var R = require('ramda');
+
 let pluck = (collection, property) => collection.map((obj) => obj[property]);
 
 var artists = [
@@ -47,15 +49,24 @@ console.log('Hello');
 //console.log(pluck(artists, 'name'));
 
 let mix = (...ingredients) => console.log('mixing', ...ingredients);
-
-let bake = () => console.log('baking');
-let cool = () => console.log('cooling');
+let bake = (...ingredients) => console.log('baking', ...ingredients);
+let cool = (...ingredients) => console.log('cooling', ...ingredients);
 
 let makeBread = (...ingredients) => {
     mix(...ingredients);
-    bake();
-    cool();
-}
+    bake(...ingredients);
+    cool(...ingredients);
+};
+
+let dmBake = R.compose(cool, bake, mix);
+
+console.log('dmbake');
+dmBake('flour', 'eggs');
+console.log('dmbake - done');
+
+console.log('make bread orig');
+makeBread('flour', 'sugar', 'water');
+console.log('make bread orig - done');
 
 let before = (fn, decoration) => (...args) => {
     decoration(...args);
@@ -75,7 +86,10 @@ let bakeBread = before(bake, mix);
 //     cool();
 // }
 
-let makeBread3 =  after(bakeBread, cool);
+let makeBread3 = after(bakeBread, cool);
+
+//console.log('makeBread3');
+//console.log(makeBread3('flour', 'sugar', 'water'));
 
 let beforeWith = (decoration) => rightApply(before, decoration);
 
@@ -83,8 +97,53 @@ let mixBefore = beforeWith(mix);
 
 let bakeBread2 = mixBefore(bake);
 
+//console.log('bakeBread2');
+//console.log(bakeBread2('flour', 'sugar', 'water'));
+
 let afterWith = (decoration) => rightApply(after, decoration);
 
 let coolAfter = afterWith(after);
 
 let makeBread2 = coolAfter(bake);
+//console.log('makeBread2');
+//console.log(makeBread2('flour', 'sugar', 'water'));
+
+
+var pickIndexes = R.compose(R.values, R.pick);
+console.log(pickIndexes([0, 2], ['a', 'b', 'c'])); // => ['a', 'c']
+
+let add = (x, y) => {
+    return x + y;
+}
+
+let addPartial = (x) => (y) => {
+    return x + y;
+}
+
+let add8 = addPartial(8);
+
+
+let add3 = (a, b, c) => {
+    return a + b + c;
+}
+
+let add3Curry = (a) => (b) => (c) => {
+    return a + b + c;
+}
+
+let g = add3Curry(1); // This returns a function with 2 parameters
+//
+//	(b) => (c) => { return 1 + b + c; }
+//
+
+
+let h = g(2); // This returns a function with 1 parameter
+//
+//	(2) => (c) => { return 1 + b + c; }
+//	       (c) => { return 1 + 2 + c; }
+//
+
+
+
+
+
