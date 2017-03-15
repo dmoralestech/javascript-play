499619de4510987a72af1eb42e7b1d8734547e89
darwinmorales <darwin.morales.tech@gmail.com>
2017-03-15 18:47:13 +1100

    monad sample from coglan

--- src/webapp/monad_example.js
+++ src/webapp/monad_example.js
@@ -3,33 +3,51 @@
  */
 
 
-var sine = function(x) {
+var sine = function (x) {
     return [Math.sin(x), 'sine was called.'];
 };
 
-var cube = function(x) {
+var cube = function (x) {
     return [x * x * x, 'cube was called.'];
 };
 
-var compose = function(f, g) {
-    return function(x) {
+var compose = function (f, g) {
+    return function (x) {
         return f(g(x));
     };
 };
 
 //console.log(compose(sine, cube)(3));
 
-var composeDebuggable = function(f, g) {
-    return function(x) {
+var composeDebuggable = function (f, g) {
+    return function (x) {
         var gx = g(x),
-            y  = gx[0],
-            s  = gx[1],
+            y = gx[0],
+            s = gx[1],
             fy = f(y),
-            z  = fy[0],
-            t  = fy[1];
+            z = fy[0],
+            t = fy[1];
 
         return [z, s + t];
     };
 };
 
 console.log(composeDebuggable(sine, cube)(3));
+
+var bind = function (f) {
+    return function (tuple) {
+        var x = tuple[0],
+            s = tuple[1],
+            fx = f(x),
+            y = fx[0],
+            t = fx[1];
+
+        return [y, s + t];
+    };
+};
+
+var f = compose(bind(sine), bind(cube));
+var unit = function(x) { return [x, ''] };
+
+// console.log(f(unit(3)));
+console.log(compose(f, unit)(3));
