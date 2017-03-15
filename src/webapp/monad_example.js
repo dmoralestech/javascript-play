d65c0d9ec57ad21d2ed9ae45522f22c12f243a79
darwinmorales <darwin.morales.tech@gmail.com>
2017-03-15 18:43:51 +1100

    monad sample from coglan

--- src/webapp/monad_example.js
+++ src/webapp/monad_example.js
@@ -3,6 +3,33 @@
  */
 
 
-var sine = function(x) { return Math.sin(x) };
+var sine = function(x) {
+    return [Math.sin(x), 'sine was called.'];
+};
 
-var cube = function(x) { return x * x * x };
+var cube = function(x) {
+    return [x * x * x, 'cube was called.'];
+};
+
+var compose = function(f, g) {
+    return function(x) {
+        return f(g(x));
+    };
+};
+
+//console.log(compose(sine, cube)(3));
+
+var composeDebuggable = function(f, g) {
+    return function(x) {
+        var gx = g(x),
+            y  = gx[0],
+            s  = gx[1],
+            fy = f(y),
+            z  = fy[0],
+            t  = fy[1];
+
+        return [z, s + t];
+    };
+};
+
+console.log(composeDebuggable(sine, cube)(3));
